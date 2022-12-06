10a1ae2a49064a8ad765276f14c6e19dd026634a
Amiko8 <119101599+Amiko8@users.noreply.github.com>
2022-12-06 22:36:17 +0300

    Add files via upload

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Введите число  ");
+string inputValue = Console.ReadLine();
+int value = Convert.ToInt32(inputValue);
+
+if (value % 2 ==1)
+{
+    System.Console.WriteLine("Число " + value + " является нечётным");
+}
+else
+{
+   System.Console.WriteLine("Число " + value + " является чётным"); 
+}
+
