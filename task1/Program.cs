c4cfdc7faa49a8c91cd055c412dc1afda5c0852b
Amiko8 <119101599+Amiko8@users.noreply.github.com>
2022-12-06 22:34:09 +0300

    Add files via upload

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = -9;
+int b = -3;
+
+int max = a;
+int min = a;
+
+if (a > max) max = a;
+if (a < max) min = a;
+
+if (b > max) max = b;
+if (b < max) min = b;
+
+Console.Write ("max= ");
+Console.WriteLine(max);
+
+Console.Write ("min= ");
+Console.WriteLine(min);
