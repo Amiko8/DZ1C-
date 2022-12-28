92f278d289bdd473d84324eddc7f40734bdeed96
Amiko8 <119101599+Amiko8@users.noreply.github.com>
2022-12-28 08:49:26 +0300

    Update Program.cs

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число: ");
+//Напишите программу, оторая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
 int n = int.Parse(Console.ReadLine());
     
 for (int i = 1; i <= n; i % 2 == 0)
